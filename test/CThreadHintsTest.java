1157a0aba272d8f47b8486c59a48c8b0cb24e484
Frode Aannevik <frode.aa@gmail.com>
2018-10-03 23:24:46 +0200

    test CThreadHints - constructor
    ref: https://github.com/sam46/Tachyon/issues/1

---
+++ test/CThreadHintsTest.java
@@ -0,0 +1,32 @@
+import org.junit.Test;
+
+import java.net.URL;
+import java.nio.file.Paths;
+import java.security.ProtectionDomain;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class CThreadHintsTest {
+
+  @Test
+  public void testConstructor() {
+    String url = "https://storage.googleapis.com/..../work-at-vimeo-2.mp4";
+    long fsize = 1007190762L;
+    long chunkSize = (long) Math.ceil(fsize * 1.0 / 5);
+    boolean acceptR = true;
+
+    ProtectionDomain domain = CThreadHintsTest.class.getProtectionDomain();
+    URL classUrl = domain.getCodeSource().getLocation();
+    String path = classUrl.getPath();
+
+    CThreadHints hints = new CThreadHints(url, fsize, chunkSize, acceptR, path);
+
+    assertEquals(url, hints.URL);
+    assertEquals(fsize, hints.fsize);
+    assertEquals(chunkSize, hints.chunkSize);
+    assertTrue(hints.acceptsRanges);
+    assertEquals(Paths.get(path).getParent().toAbsolutePath().toString(), hints.dir);
+    assertEquals(Paths.get(path).getFileName().toString(), hints.fname);
+  }
+}
